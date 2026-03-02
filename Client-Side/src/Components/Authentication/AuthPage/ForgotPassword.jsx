680c7687ab5d7d6eb00730ea6b8f67a0c1a562be
abdullahalnoman003 <noman22622@gmail.com>
2026-03-03 00:08:45 +0600

    Add authentication components: ForgotPassword, Login, and Register

---
+++ Client-Side/src/Components/Authentication/AuthPage/ForgotPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForgotPassword = () => {
+    return (
+        <div>
+            This is if anyone forgot password
+        </div>
+    );
+};
+
+export default ForgotPassword;
---
+++ Client-Side/src/Components/Authentication/AuthPage/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            This is Login Page for login.
+        </div>
+    );
+};
+
+export default Login;
