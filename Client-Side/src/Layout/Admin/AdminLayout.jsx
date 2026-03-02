0c79638b19e0893d1bb44f4a8d216ecafda38fe9
abdullahalnoman003 <noman22622@gmail.com>
2026-03-03 00:10:04 +0600

    Add AdminLayout and HomeLayout components for admin and home page layouts

---
+++ Client-Side/src/Layout/Admin/AdminLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminLayout = () => {
+    return (
+        <div>
+            This Is admin page layout!
+        </div>
+    );
+};
+
+export default AdminLayout;
