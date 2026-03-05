a30ab1581a1e1fa9cbd1281ad14d3965501fc53b
abdullahalnoman003 <noman22622@gmail.com>
2026-03-06 05:11:11 +0600

    Implement authentication context with user state management and loading indicator; add profile component; enhance router with login and register paths; integrate toast notifications in main app.

--- Client-Side/src/Components/Authentication/Context/AuthProvider.jsx
+++ Client-Side/src/Components/Authentication/Context/AuthProvider.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../../../Firebase/firebase.init";
 import { AuthContext } from "./AuthContext";
 
 const AuthProvider = ({ children }) => {
-  const authInfo = {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (loggedInUser) => {
+      if (loggedInUser) {
+        try {
+          const token = await loggedInUser.getIdToken(); 
+          localStorage.setItem("access-token", token); 
+          setUser(loggedInUser);
+        } catch (error) {
+          console.error("Token fetch error:", error);
+        }
+      } else {
+        localStorage.removeItem("access-token");
+        localStorage.removeItem("userRole");
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("userRole");
+    localStorage.removeItem("access-token");
+    signOut(auth);
   };
+  const authInfo = {
+    user,
+    loading,
+    logout,
+  };
+
+  if (loading) {
+    return (
+      <div className="h-screen w-full flex items-center justify-center bg-gradient-to-br from-base-100 via-base-200 to-base-300">
+        <div className="text-center space-y-3">
+          <span className="loading loading-bars loading-lg text-primary"></span>
+          <p className="text-xl font-semibold text-primary ">Please Wait...</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
